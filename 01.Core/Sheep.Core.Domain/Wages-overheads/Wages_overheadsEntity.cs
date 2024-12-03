223133c998b66044beee934edd390f1627b74a4b
ali mohamadi <ali.mohamadi.1988@gmail.com>
2024-12-03 21:40:59 +0330

    price to do

---
+++ 01.Core/Sheep.Core.Domain/Wages-overheads/Wages_overheadsEntity.cs
@@ -0,0 +1,32 @@
+﻿using Sheep.Framework.Domain.Entities;
+
+
+namespace Sheep.Core.Domain.Wages_overheads
+{
+    public class Wages_overheadsEntity: BaseEntity<Guid>
+    {
+        public long Salary { get; set; }
+        public long Overhead { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public Wages_overheadsEntity() { }  
+        public Wages_overheadsEntity(long salary, long overhead)
+        {
+            Salary = salary;
+            Overhead = overhead;
+            IsDeleted = false;
+
+        }
+        public void Edit( long salary, long overhead)
+        {
+            Salary = salary;
+            Overhead = overhead;
+        }
+        public void Delete()
+        {
+            IsDeleted = true;
+        }
+
+    }
+}
